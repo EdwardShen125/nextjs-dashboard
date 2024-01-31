9582572e7b29036db1e0fcd146acb1364227cd4a
EdwardShen125 <495430312@qq.com>
2024-01-31 11:26:24 +0800

    🎸feat(invoice/update): add update error handler

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,9 @@ import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
 
 const FormSchema = z.object({
-    id: z.string(),
+    id: z.string({
+        invalid_type_error: 'Please select an invoice id.',
+    }),
     customerId: z.string({
         invalid_type_error: 'Please select a customer.',
     }),
@@ -65,12 +67,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const {customerId, amount, status} = UpdateInvoice.parse(
-        Object.fromEntries(
-            formData.entries()
-        )
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse(
+        {
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        }
     );
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
 
     const amountInCents = amount * 100;
 
